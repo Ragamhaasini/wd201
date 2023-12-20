4930bf80c009c2b22db17a46a199a07f7a4b8b75
Ragamhaasini <151642741+Ragamhaasini@users.noreply.github.com>
2023-12-20 23:11:20 +0530

    Update registration.js

--- http-server/registration.js
+++ http-server/registration.js
@@ -1,90 +1,70 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Registration Form</title>
-    <style>
-      .form-box {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-      }
-      form {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        width: 50%;
-        padding: 20px;
-        border: 1px solid skyblue;
-      }
-      table {
-        margin-top: 20px;
-        border-collapse: collapse;
-        width: 50%;
-        text-align: center;
-      }
-      th, td {
-        border: 1px solid black;
-        padding: 8px;
-      }
-      th {
-        background-color: grey;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="form-box">
-      <form id="registration-form">
-        <h1>Registration Form</h1>
-        <table>
-          <tbody>
-            <tr>
-              <td>Name:</td>
-              <td><input type="text" id="name" required></td>
-            </tr>
-            <tr>
-              <td>Email:</td>
-              <td><input type="email" id="email" required></td>
-            </tr>
-            <tr>
-              <td>Password:</td>
-              <td><input type="password" id="password" required></td>
-            </tr>
-            <tr>
-              <td>Dob:</td>
-              <td><input type="date" id="dob" required></td>
-            </tr>
-            <tr>
-              <td>Accepted terms?</td>
-              <td><input type="checkbox" id="terms" required></td>
-            </tr>
-          </tbody>
-        </table>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-<div class="table-box">
-  <table id="userTable">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Password</th>
-        <th>dob</th>
-        <th>accepted terms?</th>
-      </tr>
-    </thead>
-    <tbody></tbody>
-  </table>
-</div>
+const form = document.getElementById('registration-form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const dobInput = document.getElementById('dob');
+const termsInput = document.getElementById('terms');
+const userTable = document.getElementById('userTable').getElementsByTagName('tbody')[0];
 
-<script src="index.js"></script>
-  </body>
-</html>
-<style>
-  .table-box {
-    display: flex;
-    justify-content: center;
-    margin-top: 50px;
+// Load saved data from web storage, if available
+let savedData = JSON.parse(localStorage.getItem('registrationData'));
+if (savedData) {
+  savedData.forEach((data) => {
+    const newRow = userTable.insertRow();
+    const nameCell = newRow.insertCell(0);
+    const emailCell = newRow.insertCell(1);
+    const passwordCell = newRow.insertCell(2);
+    const dobCell = newRow.insertCell(3);
+    const termsCell = newRow.insertCell(4);
+    nameCell.innerHTML = data.name;
+    emailCell.innerHTML = data.email;
+    passwordCell.innerHTML = data.password;
+    dobCell.innerHTML = data.dob;
+    termsCell.innerHTML = data.termsAccepted ? 'true' : 'false';
+  });
+} else {
+  savedData = [];
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Get user's date of birth and calculate age
+  const dob = new Date(dobInput.value);
+  const ageInMilliseconds = Date.now() - dob.getTime();
+  const ageInYears = ageInMilliseconds / 1000 / 60 / 60 / 24 / 365;
+
+  // Check if user is between 18 and 55 years old
+  if (ageInYears < 18 || ageInYears > 55) {
+    alert('You must be between 18 and 55 years old to register.');
+    return;
   }
-</style> 
+
+  const formData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    password: passwordInput.value,
+    dob: dobInput.value,
+    termsAccepted: termsInput.checked,
+  };
+
+  // Save form data to web storage
+  savedData.push(formData);
+  localStorage.setItem('registrationData', JSON.stringify(savedData));
+
+  // Append form data to user table
+  const newRow = userTable.insertRow();
+  const nameCell = newRow.insertCell(0);
+  const emailCell = newRow.insertCell(1);
+  const passwordCell = newRow.insertCell(2);
+  const dobCell = newRow.insertCell(3);
+  const termsCell = newRow.insertCell(4);
+  nameCell.innerHTML = formData.name;
+  emailCell.innerHTML = formData.email;
+  passwordCell.innerHTML = formData.password;
+  dobCell.innerHTML = formData.dob;
+  termsCell.innerHTML = formData.termsAccepted ? 'true' : 'false';
+
+  // Reset form
+  form.reset();
+});
